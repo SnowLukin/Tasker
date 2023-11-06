d462032100a08c94ac61f19a7cb6337744ff898a
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-11-06 23:59:44 +0300

    Initial Commit

---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,32 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Snow Lukin on 06.11.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TaskerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
